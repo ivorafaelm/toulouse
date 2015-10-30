4cd215bf4421518ec230d89abb846ae3599947e0
Ivo Rafael Santos Menezes <ivorafaelm@gmail.com>
2015-10-30 08:55:32 -0200

    Commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+var paths = {
+    'jquery': './vendor/bower_components/jquery/',
+    'bootstrap': './vendor/bower_components/bootstrap-sass/assets/',
+    'onepagescroll': './vendor/bower_components/javascripts/onepagescroll/src/jquery.onepage-scroll.js'
+}
+
+elixir(function(mix) {
+    mix.sass("app.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
+        .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+        .copy(paths.onepagescroll, 'public/js')
+        .scripts([
+            paths.jquery + "dist/jquery.js",
+            paths.bootstrap + "javascripts/bootstrap.js"
+        ], 'public/js/app.js', './');
+});
